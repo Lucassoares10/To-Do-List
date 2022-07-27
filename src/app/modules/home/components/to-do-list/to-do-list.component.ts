954a99d2e1011146a1ad7b441eec1313dbae416e
Lucas Soares <80491974+Lucassoares10@users.noreply.github.com>
2022-07-26 21:11:14 -0300

    Adicionando estilização e criando a interface model

--- src/app/modules/home/components/to-do-list/to-do-list.component.ts
+++ src/app/modules/home/components/to-do-list/to-do-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoListComponent implements OnInit {
 
+  public task: Array<{task: string,checked: boolean}> = [];
   constructor() { }
 
   ngOnInit(): void {
